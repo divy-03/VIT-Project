28c9134c598f1dbec9910306e5e5594f636b8db4
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-10-05 16:19:37 +0530

    adjustedbanner

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import "./Home.css";
+import HeadBanner from "../layout/Header/HeadBanner";
 
 const Home = () => {
   return (
     <Fragment>
+      <HeadBanner />
       <div className="Landing-main">
-        <pre>        Hello </pre>
+        <pre> Hello </pre>
         <p className="XL">X</p>
         <pre>changers</pre>
       </div>
--- src/component/layout/Header/Header.js
+++ src/component/layout/Header/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "./Navbar.js";
-import HeadBanner from "./HeadBanner.js";
 
 const Header = () => {
   return (
     <>
       <Navbar />
-      <HeadBanner />
     </>
   );
 };
